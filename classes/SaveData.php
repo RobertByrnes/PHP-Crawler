ef4e8f9e026b770eec67f63c9fd35860d84f3517
RobByrnes <robbyrnes@hotmail.co.uk>
2021-03-12 08:49:17 +0000

    add strict types to savedata

--- classes/SaveData.php
+++ classes/SaveData.php
@@ -1,17 +1,36 @@
 <?php
 
+declare(strict_types=1);
 class SaveData
 {
+    /**
+     * file path to queue.txt
+     * @var string 
+     */
     public string $queue_path;
+
+    /**
+     * project name used as directory name in results dir
+     * @var string 
+     */
     public string $crawled_path;
 
+    /**
+     * class SaveData constructor
+     * @param string $project_name 
+     */
     public function __construct($project_name)
     {
         $this->crawled_path = "results/".$project_name."/crawled.txt";
         $this->queue_path = "results/".$project_name."/queued.txt";
     }
 
-    public function create_dir($directory)
+    /**
+     * creates the project dir within results in the root
+     * @param string $directory
+     * @return void
+     */
+    public function create_dir($directory) : void
     {
         try {
             if (!is_dir($directory)) {
@@ -25,7 +44,11 @@ class SaveData
         }
     }
 
-    public function create_files()
+    /**
+     * creates queue.txt and crawled.txt in the project dir
+     * @return void
+     */
+    public function create_files() : void
     {
         try {
             if (!file_exists($this->crawled_path)) {
@@ -39,11 +62,14 @@ class SaveData
         catch (Exception $e) {
             printf("[+] ".$e."\n");
         }
-        
-
     }
 
-    public function file_to_array(string $file_name)
+    /**
+     * opens either queue.txt or crawled.txt and parses stream to array
+     * @param string $file_name
+     * @return array
+     */
+    public function file_to_array(string $file_name) : array
     {
         $results = [];
         $file = fopen($file_name, "r");
@@ -60,31 +86,58 @@ class SaveData
             preg_replace("\n", "", $line);
             $result[] = $line;
         }
+        fclose($file);
         return $results;
     }
 
-    public function array_to_file(array $links, $file_name)
+    /**
+     * takes queue array or crawled array and writes to queue.txt or crawled.txt
+     * @param array $links
+     * @param string $file_name
+     * @return void
+     */
+    public function array_to_file(array $links, string $file_name) : void
     {
-        $emptied_file = fopen(file_name, "w");
+        $emptied_file = fopen($file_name, "w");
         asort($links);
         foreach ($links as $link) {
             fwrite($emptied_file, $link."\n");
         }
+        fclose($emptied_file);
     }
 
-    public function write_file($path, $data)
+    /**
+     * uses fwrite to open new files, or open - empty - then rewrite
+     * @param string $path
+     * @param string $data
+     * @return void
+     */
+    public function write_file(string $path, string $data) : void
     {
         $file = fopen($path, "w");
         fwrite($file, $data."\n");
+        fclose($file);
     }
 
-    public function append_to_file($path, $data)
+    /**
+     * appends lines to either queue.txt or crawled.txt
+     * @param string $path
+     * @param string $data
+     * @return void
+     */
+    public function append_to_file(string $path, string $data) : void
     {
         $file = fopen($path, "a");
         fwrite($file, $data."\n");
+        fclose($file);
     }
 
-    public function delete_file_contents($path)
+    /**
+     * empties files - unused in PHP-Crawler
+     * @param string $path
+     * @return void
+     */
+    public function delete_file_contents(string $path) : void
     {
         $file = fopen($path, "w");
         fclose($file);
